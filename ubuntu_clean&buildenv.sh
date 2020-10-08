58a3bb4d7f3cc15b2656bc35eb71cff01e2df78f
Baili <2389730775@qq.com>
2020-10-07 21:54:12 -0500

    Add files via upload

---
+++ ubuntu_clean&buildenv.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+docker rmi `docker images -q`
+sudo rm -rf /usr/share/dotnet /etc/mysql /etc/php /etc/apt/sources.list.d
+sudo -E apt-get -y purge azure-cli ghc* zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql* php*
+sudo -E apt-get update
+sudo -E apt-get -y autoremove --purge
+sudo -E apt-get clean 
+
+
+sudo apt install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev tree lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip jq
+
+mkdir ~/bin
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+PATH=~/bin:$PATH
