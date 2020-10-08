2ec2bb1237d8a332d639336f8c43e48206bcb737
Baili <2389730775@qq.com>
2020-10-08 10:58:24 +0800

    Update ubuntu_clean&buildenv.sh

--- ubuntu_clean&buildenv.sh
+++ ubuntu_clean&buildenv.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker rmi `docker images -q`
-sudo rm -rf /usr/share/dotnet /etc/mysql /etc/php /etc/apt/sources.list.d
+#sudo rm -rf /usr/share/dotnet /etc/mysql /etc/php /etc/apt/sources.list.d
 sudo -E apt-get -y purge azure-cli ghc* zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql* php*
 sudo -E apt-get update
 sudo -E apt-get -y autoremove --purge
