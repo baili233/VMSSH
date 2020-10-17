5b1d21d8343835ce8a1f514c1471a69f68b7c1e7
Baili <2389730775@qq.com>
2020-10-17 10:34:41 +0800

    Update ubuntu_clean&buildenv.sh

--- ubuntu_clean&buildenv.sh
+++ ubuntu_clean&buildenv.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker rmi `docker images -q`
-#sudo rm -rf /usr/share/dotnet /etc/mysql /etc/php /etc/apt/sources.list.d
+sudo rm -rf /usr/share/dotnet /etc/mysql /etc/php /etc/apt/sources.list.d
 sudo -E apt-get -y purge azure-cli ghc* zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql* php*
 sudo -E apt-get update
 sudo -E apt-get -y autoremove --purge
@@ -11,5 +11,5 @@ sudo apt install git-core gnupg flex bison gperf build-essential zip curl zlib1g
 
 mkdir ~/bin
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
-chmod a+x ~/bin/repo
+sudo chmod a+x ~/bin/repo
 PATH=~/bin:$PATH
